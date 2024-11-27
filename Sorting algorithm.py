bd19bd7397ed5f319fcc9e84f0e20e617680e9fa
sanjana satesh <Sanjanasatesh8@gmail.com>
2024-11-26 19:51:51 -0500

    Add files via upload

---
+++ Sorting algorithm.py
@@ -0,0 +1,54 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+if __name__ == "__main__":
+    sample_list = [64, 34, 25, 12, 22, 11, 90]
+    print("Unsorted list:", sample_list)
+    print("Bubble Sort:", bubble_sort(sample_list.copy()))
+    print("Merge Sort:", merge_sort(sample_list.copy()))
+    print("Quick Sort:", quick_sort(sample_list.copy()))
